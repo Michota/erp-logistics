fc9bc040f3f461d05af6ef61ddc87ce9b7b7f2e6
Michota <dawidmichota@gmail.com>
2025-01-29 22:40:16 +0100

    fix: resolve alias for @ path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import path from "path";
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
