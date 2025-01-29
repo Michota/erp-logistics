c7d65a11cdabbd30ade9affebb7d47962413af3a
Michota <dawidmichota@gmail.com>
2025-01-29 22:49:12 +0100

    refactor: shorten name of dark mode methods

--- src/shared/hooks/useDarkMode.ts
+++ src/shared/hooks/useDarkMode.ts
@@ -1,11 +1,11 @@
-const htmlTag = document.documentElement;
+const bodyTag = document.body;
 
 const DARK_MODE_SELECTOR = "dark";
 
 function useDarkMode() {
-  const enable = () => htmlTag.classList.add(DARK_MODE_SELECTOR);
-  const disable = () => htmlTag.classList.remove(DARK_MODE_SELECTOR);
-  const toggle = () => htmlTag.classList.toggle(DARK_MODE_SELECTOR);
+  const enable = () => bodyTag.classList.add(DARK_MODE_SELECTOR);
+  const disable = () => bodyTag.classList.remove(DARK_MODE_SELECTOR);
+  const toggle = () => bodyTag.classList.toggle(DARK_MODE_SELECTOR);
 
   return { enable, disable, toggle };
 }
