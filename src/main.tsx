1ee6d354dc3b45ed2aadd7c73f9a74fc413aae08
Michota <dawidmichota@gmail.com>
2025-01-29 22:38:13 +0100

    fix: import styles for main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
+// Import styles
+import "./main.css"
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
