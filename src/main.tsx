ac1a2c16227b8431a48d674fd0eb1c49c4584709
Michota <dawidmichota@gmail.com>
2025-01-29 23:39:43 +0100

    fix: rename main.css to index.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 // Import styles
-import "./main.css"
+import "./index.css"
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
