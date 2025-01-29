6fb4de3b90906c3a85ef40a3fa7fbefb43fb0f06
Michota <dawidmichota@gmail.com>
2025-01-29 23:35:44 +0100

    feat: improve the way links are rendered

--- src/routes/components/NavBar.tsx
+++ src/routes/components/NavBar.tsx
@@ -1,13 +1,24 @@
 import { cn } from "@/lib/utils";
-import { Link } from "@tanstack/react-router";
+import { Link, LinkProps } from "@tanstack/react-router";
+import { HomeIcon, LucideIcon, RouteIcon } from "lucide-react";
 
-// TODO: render navigation buttons imperatively
+interface NavBarLink extends LinkProps {
+  title: string;
+  icon: LucideIcon;
+}
+
+const LINKS: NavBarLink[] = [
+  { to: "/", title: "Home", icon: HomeIcon },
+  { to: "/route", title: "Route", icon: RouteIcon },
+];
+
 export function NavBar() {
-  return (
-    <nav className={cn("nav-bar", "flex gap-2")}>
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>
-    </nav>
-  );
+    return <nav className={cn("nav-bar", "flex gap-2 ")}>{LINKS.map(NavBarIcon)}</nav>;
+}
+
+function NavBarIcon({ icon: Icon, title, to }: NavBarLink) {
+  return <Link to={to} className="[&.active]:font-bold">
+    {<Icon />}
+    {title}
+  </Link>;
 }
