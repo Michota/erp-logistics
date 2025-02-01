9541474dfeb42e9d4f734ec72e9cf66856fe8d6c
Michota <dawidmichota@gmail.com>
2025-02-01 21:48:28 +0100

    wip: display route on map

--- src/routes/~route/~route.tsx
+++ src/routes/~route/~route.tsx
@@ -1,36 +1,57 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
-import { WaypointsList } from "./~$routeId/components/WaypointsList";
 import { Button } from "@/components/ui/button";
-import { RouteIcon } from "lucide-react";
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { LatLng, LatLngExpression, point, routing } from "leaflet";
+import { RouteIcon } from "lucide-react";
+import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
+import { WaypointsList } from "./~$routeId/components/WaypointsList";
+import { useEffect } from "react";
+import L from "leaflet";
+import "leaflet-routing-machine";
+import { createControlledLayer } from "react-leaflet/LayersControl";
+
 export const Route = createFileRoute("/route")({
   component: RouteComponent,
 });
 
 const points: [number, number][] = [
-  [50.80904029689233, 20.34840150520644],
   [50.95857599800388, 20.25370827892464],
-  [51.010918206658374, 20.802236114093613],
+  [50.80904029689233, 20.34840150520644],
   [50.86683508347538, 20.62555240399504],
+  [51.010918206658374, 20.802236114093613],
 ];
+
+const RoutingMachine = ({ waypoints }: { waypoints: LatLng[] }) => {
+  const map = useMap();
+
+  const routingControl = routing.control({ addWaypoints: false, waypoints, show: false });
+
+  map.addControl(routingControl);
+
+  return null;
+};
 
 function RouteComponent() {
   const summedPosition: [number, number] = points.reduce((acc, cur) => [acc[0] + cur[0], acc[1] + cur[1]], [0, 0]);
 
-  const center: [number, number] = [
-    summedPosition[0] / points.length,
-    summedPosition[1] / points.length,
-  ];
-  console.log("🚀 ~ file: ~route.tsx:26 ~ RouteComponent ~ center:", center);
+  const center: [number, number] = [summedPosition[0] / points.length, summedPosition[1] / points.length];
+
   return (
     <div className="w-[50rem] h-[50rem] bg-red-400">
       Hello "/route"!
-      <MapContainer style={{ height: "30rem" }} center={center} zoom={10} scrollWheelZoom={false}>
+      <MapContainer
+        className="mapa"
+        style={{ height: "100%", width: "40rem" }}
+        center={center}
+        zoom={10}
+        scrollWheelZoom={false}
+      >
         <TileLayer
+          zIndex={0}
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <RoutingMachine waypoints={points.map(L.latLng)} />
         {points.map((point) => (
           <Marker key={point.join("-")} position={point} />
         ))}
