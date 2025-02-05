91ecc6e5384175d354cba5a272b88f5f9a9541e4
Michota <dawidmichota@gmail.com>
2025-02-05 23:15:54 +0100

    docs: add comment

--- src/types/waypoint.ts
+++ src/types/waypoint.ts
@@ -1,5 +1,7 @@
 import { RoutePoint } from "./routePoints";
 
+// TODO: this should be named Point actually...
+// The waypoint suggest, that its a point within a order of points
 export interface Waypoint extends RoutePoint {
   coordinates: { lat: number; lng: number };
 }
